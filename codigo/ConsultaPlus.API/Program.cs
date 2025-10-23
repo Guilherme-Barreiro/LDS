5a4da3dd8fc672a007e207c08aae3e37c71aafe4
Guilherme Barreiro <80386801+Guilherme-Barreiro@users.noreply.github.com>
2025-10-23 16:59:33 +0100

    testing pipeline

--- codigo/ConsultaPlus.API/Program.cs
+++ codigo/ConsultaPlus.API/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//OLA
 
 var app = builder.Build();
 
